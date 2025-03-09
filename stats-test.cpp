4f0cc506b02f3eb5b71bd513f30e7a1da12c6b5c
Sudeep Prasad <sudeep.rp@gmail.com>
2025-03-09 13:35:00 +0530

    stats expectations

--- stats-test.cpp
+++ stats-test.cpp
@@ -16,19 +16,6 @@ TEST(Statistics, AverageNaNForEmpty) {
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
-    //Design the REQUIRE statement here.
+    //Design the EXPECT statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
-}
-
-TEST(Alert, AlertsWhenMaxExceeds) {
-    EmailAlert emailAlert;
-    LEDAlert ledAlert;
-    std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
-    
-    const float maxThreshold = 10.2;
-    StatsAlerter statsAlerter(maxThreshold, alerters);
-    statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
-
-    EXPECT_TRUE(emailAlert.emailSent);
-    EXPECT_TRUE(ledAlert.ledGlows);
 }
